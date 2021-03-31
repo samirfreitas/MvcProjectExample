a75f8b1ba09b936567308b5bb9c3729bf55932d9
samir freitas de souza <samir.freitas@am4.com.br>
2021-03-31 10:02:38 -0300

    Adjusts Views

---
+++ src/Example.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string DocumentFormat(this RazorPage page, int tipoPessoa, string document)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(document).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(document).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
