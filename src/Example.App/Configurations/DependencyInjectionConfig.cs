aea5a07ae8b78fda748e327da9fe4e1874a23da6
samir freitas de souza <samir.freitas@am4.com.br>
2021-03-31 21:20:56 -0300

    startup organization

---
+++ src/Example.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using Example.App.Extensions;
+using Example.Business.Intefaces;
+using Example.Data.Context;
+using Example.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.App.Configurations
+{
+    public static class DependencyInjectionConfig 
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ExampleDbContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IVendorRepository, VendorRepository>();
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, CoinValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+
+}
---
+++ src/Example.App/Configurations/MvcConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.App.Configurations
+{
+    public static class MvcConfig
+    {
+        public static IServiceCollection AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddMvc(o => {
+                o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O Valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(x => "O Valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "O Valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "E necessario que o body na requisicao nao esteja vazio.");
+                o.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor((x) => "O valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numerico.");
+                o.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor((x) => "O valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetValueIsInvalidAccessor((x) => "O valor preenchido e invalido para este campo.");
+                o.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor((x) => "Este campo precisa ser preenchido.");
+                o.ModelBindingMessageProvider.ValueMustBeANumberAccessor("O Campo deve ser numerico.");
+
+            });
+
+            return services;
+        }
+    }
+
+}
