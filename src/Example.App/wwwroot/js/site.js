44b36cf6f6353ec00a4f7313c51f1f32ce9f7232
samir freitas de souza <samir.freitas@am4.com.br>
2021-03-31 21:21:38 -0300

    Async search zipcode

--- src/Example.App/wwwroot/js/site.js
+++ src/Example.App/wwwroot/js/site.js
@@ -45,3 +45,55 @@
     });
 
 };
+
+
+function SearchCep() {
+    $(document).ready(function () {
+
+        function clear_form_cep() {
+            $('#Address_PublicPlace').val('');
+            $('#Address_District').val('');
+            $('#Address_City').val('');
+            $('#Address_State').val('');
+        }
+
+        $('#Address_ZipCode').blur(function () {
+        
+            var zipCode = $(this).val().replace(/\D/g, '');
+
+            if (zipCode != "") {
+                var validateZipCode = /^[0-9]{8}$/;
+
+                if (validateZipCode.test(zipCode)) {
+
+                    $('#Address_PublicPlace').val('...');
+                    $('#Address_District').val('...');
+                    $('#Address_City').val('...');
+                    $('#Address_State').val('...');
+
+
+                    $.getJSON("https://viacep.com.br/ws/" + zipCode + "/json/?callback=?", function (dados) {
+
+                        if (!("erro" in dados)) {
+
+                            $('#Address_PublicPlace').val(dados.logradouro);
+                            $('#Address_District').val(dados.bairro);
+                            $('#Address_City').val(dados.localidade);
+                            $('#Address_State').val(dados.uf);
+
+                        } else {
+                            clear_form_cep();
+                            Alert('CEP nao encontrado.')
+                        }
+
+                    });
+
+                }
+            } else {
+                clear_form_cep();
+                Alert('Formato de CEP invalido.')
+            }
+        });
+
+    });
+};
